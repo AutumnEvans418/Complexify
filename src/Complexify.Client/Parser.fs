1095343f3b2b083cae87f8d4736164a40f6ae45a
Chris Evans <chrisevans9629@gmail.com>
2022-08-21 01:58:14 -0500

    server works

---
+++ src/Complexify.Client/Parser.fs
@@ -0,0 +1,82 @@
+﻿module Parser
+
+open System
+open FParsec
+open FParsec.CharParsers
+
+let ws = spaces // skips any whitespace
+
+let str_ws s = pstring s >>. ws
+
+
+type BinaryOperator =
+    | Add
+    | Sub
+    | Mul
+    | Div
+    | Pow
+    | Eql
+
+type UnaryOperator =
+    | Neg
+    | Exp
+    | Log
+
+type Operator<'a> = {token:string;op:'a;wrap:bool}
+
+
+type MathExpression =
+    | Number of float
+    | Bin of MathExpression * Operator<BinaryOperator> * MathExpression
+    | Una of MathExpression * Operator<UnaryOperator>
+    | Invalid of string
+
+// we calculate with double precision floats
+let number = pfloat .>> ws |>> Number
+
+// we set up an operator precedence parser for parsing the arithmetic expressions
+let opp = new OperatorPrecedenceParser<MathExpression,unit,unit>()
+let expr = opp.ExpressionParser
+opp.TermParser <- number <|> between (str_ws "(") (str_ws ")") expr
+
+// operator definitions follow the schema
+// operator type, string, trailing whitespace parser, precedence, associativity, function to apply
+
+let add = {token="+";op=Add;wrap=true}
+let sub = {token="-";op=Sub;wrap=true}
+let mul = {token="*";op=Mul;wrap=true}
+let div = {token="/";op=Div;wrap=true}
+let pow = {token="^";op=Pow;wrap=true}
+let neg =  {token="-";op=Neg;wrap=true}
+let eql = {token="=";op=Eql;wrap=false}
+let log =  {token="log";op=Log;wrap=true}
+let exp =  {token="exp";op=Exp;wrap=true}
+
+opp.AddOperator(InfixOperator("+", ws, 2, Associativity.Left, fun x y -> Bin(x,add,y)))
+opp.AddOperator(InfixOperator("-", ws, 2, Associativity.Left, fun x y -> Bin(x, sub,y)))
+opp.AddOperator(InfixOperator("*", ws, 3, Associativity.Left, fun x y -> Bin(x, mul,y)))
+opp.AddOperator(InfixOperator("/", ws, 3, Associativity.Left, fun x y -> Bin(x, div,y)))
+opp.AddOperator(InfixOperator("^", ws, 4, Associativity.Right, fun x y -> Bin(x,pow,y)))
+opp.AddOperator(PrefixOperator("-", ws, 5, true, fun x -> Una(x,neg)))
+
+opp.AddOperator(InfixOperator("=", ws, 1, Associativity.Left, fun x y -> Bin(x,eql,y)))
+
+// we also want to accept the operators "exp" and "log", but we don't want to accept
+// expressions like "logexp" 2, so we require that non-symbolic operators are not
+// followed by letters
+
+let ws1 = nextCharSatisfiesNot isLetter >>. ws
+opp.AddOperator(PrefixOperator("log", ws1, 5, true, fun x -> Una(x,log)))
+opp.AddOperator(PrefixOperator("exp", ws1, 5, true, fun x -> Una(x,exp)))
+
+let completeExpression = ws >>. expr .>> eof // we append the eof parser to make
+                                            // sure all input is consumed
+
+// running and testing the parser
+/////////////////////////////////
+let calculate s = run completeExpression s
+
+let getTree  s=
+    match calculate s with
+    | Success (v,_,_) -> v
+    | Failure (msg,_,_) -> Invalid msg
---
+++ src/Complexify.Client/Runner.fs
@@ -0,0 +1,78 @@
+﻿module Runner
+open Parser
+open System
+
+type ExpressionResult =
+    | EqualResult of float * float * bool
+    | MathResult of float
+
+let rec doMath tree =
+
+    let get x =
+        match doMath x with
+        | MathResult v -> v
+        | _ -> nan
+
+    match tree with
+    | Number n -> MathResult n
+    | Una (x,o) -> 
+        match o.op with
+        | Neg -> MathResult(-(get x))
+        | Log -> MathResult(get x |> System.Math.Log)
+        | Exp -> MathResult (Math.Exp(get x))
+    | Bin (x, o,y) ->
+        match o.op with
+        | Mul -> MathResult(get x * get y)
+        | Add -> MathResult(get x + get y)
+        | Div -> MathResult(get x / get y)
+        | Sub -> MathResult(get x - get y)
+        | Pow -> MathResult(System.Math.Pow(get x, get y))
+        | Eql -> 
+            let v1 = get x
+            let v2 = get y
+            EqualResult (v1, v2, v1 = v2)
+    | Invalid _ -> MathResult nan
+    
+
+
+let rec displayTree tree =
+    match tree with
+    | Number n -> sprintf "%g" n
+    | Bin (x,o,y) -> 
+        if o.wrap then
+            sprintf "(%s%s%s)" (displayTree x) o.token (displayTree y)
+        else
+            sprintf "%s%s%s" (displayTree x) o.token (displayTree y)
+    | Una (x,o) ->
+        sprintf "(%s(%s))" o.token (displayTree x)
+    | Invalid msg -> msg
+
+
+let private complexExp v o1 o2 exp =
+    Bin(Bin(v, o1, exp), o2, v)
+
+let private complexExpU o1 o2 exp =
+    Una(Una(exp, o2), o1)
+
+let rec complexify tree =
+    let random = Random()
+    match tree with
+    | Una (x,o) -> Una (complexify x, o)
+    | Bin (x,o,y) -> 
+        match o.op with
+        | Eql -> Bin (complexify x, o, y)
+        | _ ->
+            let value() = Number (float(random.Next(0,100)))
+
+            let org = Bin(x,o,y)
+            //Bin(Bin(value, mul, Bin(x,o,y)), div, value)
+            complexExp (value()) mul div org 
+            |> complexExp (value()) add sub
+            
+        
+    | Invalid _ -> tree
+    | Number _ -> tree
+
+let rec complexifyRepeat (iter:int) (tree:MathExpression) =
+    if iter > 0 then complexifyRepeat (iter-1) (complexify tree)
+    else tree
